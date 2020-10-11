9dc66399ad348563b9bc75a6d659f856fec219af
Daniel Rankov <daniel.rankov@mentormate.com>
2020-10-11 19:32:30 +0300

    CloudTrail and Condig IAM

--- aws-cdk-cis/aws_cdk_cis/aws_cdk_cis_stack.py
+++ aws-cdk-cis/aws_cdk_cis/aws_cdk_cis_stack.py
@@ -45,3 +45,103 @@ class AwsCdkCisStack(core.Stack):
                                  bucket=cloudtrail_bucket,
                                  kms_key=cloudtrail_kms
                                  )
+
+        cloudtrail_kms.grant(iam.ServicePrincipal(
+            'cloudtrail.amazonaws.com'), 'kms:DescribeKey')
+
+        cloudtrail_kms.grant(iam.ServicePrincipal(
+            'cloudtrail.amazonaws.com', conditions={
+                'StringLike': {'kms:EncryptionContext:aws:cloudtrail:arn': 'arn:aws:cloudtrail:*:'+core.Stack.of(self).account+':trail/*'}
+            }), 'kms:GenerateDataKey*')
+
+        cloudtrail_kms.add_to_resource_policy(iam.PolicyStatement(
+            actions=["kms:Decrypt", "kms:ReEncryptFrom"],
+            conditions={
+                'StringEquals': {'kms:CallerAccount': core.Stack.of(self).account},
+                'StringLike': {'kms:EncryptionContext:aws:cloudtrail:arn': 'arn:aws:cloudtrail:*:'+core.Stack.of(self).account+':trail/*'}
+            },
+            effect=iam.Effect.ALLOW,
+            principals=[iam.AnyPrincipal()],
+            resources=['*']
+        ))
+
+        cloudtrail_kms.add_to_resource_policy(iam.PolicyStatement(
+            actions=["kms:CreateAlias"],
+            conditions={
+                'StringEquals': {'kms:CallerAccount': core.Stack.of(self).account,
+                                 'kms:ViaService': 'ec2.' +
+                                 core.Stack.of(self).region+'.amazonaws.com'}
+            },
+            effect=iam.Effect.ALLOW,
+            principals=[iam.AnyPrincipal()],
+            resources=['*']
+        ))
+
+        cloudtrail_kms.add_to_resource_policy(iam.PolicyStatement(
+            actions=["kms:Decrypt", "kms:ReEncryptFrom"],
+            conditions={
+                'StringEquals': {'kms:CallerAccount': core.Stack.of(self).account},
+                'StringLike': {'kms:EncryptionContext:aws:cloudtrail:arn': 'arn:aws:cloudtrail:*:'+core.Stack.of(self).account+':trail/*'}
+            },
+            effect=iam.Effect.ALLOW,
+            principals=[iam.AnyPrincipal()],
+            resources=['*']
+        ))
+
+        # config_role = iam.Role(self, "ConfigRole",
+        #                        assumed_by=iam.ServicePrincipal(
+        #                            'config.amazonaws.com'),
+        #                        managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name(
+        #                            'service-role/AWS_ConfigRole')]
+        #                        )
+
+        config_role = iam.CfnServiceLinkedRole(self,
+                                               id='ServiceLinkedRoleConfig',
+                                               aws_service_name='config.amazonaws.com'
+                                               )
+
+        global_config = config.CfnConfigurationRecorder(self, 'ConfigRecorder',
+                                                        name='default',
+                                                        # role_arn=config_role.role_arn,
+                                                        role_arn="arn:aws:iam::" + \
+                                                        core.Stack.of(
+                                                            self).account+":role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig",
+                                                        # role_arn=config_role.get_att(
+                                                        #     attribute_name='resource.arn').to_string(),
+                                                        recording_group=config.CfnConfigurationRecorder.RecordingGroupProperty(
+                                                            all_supported=True,
+                                                            include_global_resource_types=True
+                                                        )
+                                                        )
+        config_bucket = s3.Bucket(self, "ConfigS3",
+                                  block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+                                  encryption=s3.BucketEncryption.S3_MANAGED,
+                                  removal_policy=core.RemovalPolicy.DESTROY,
+                                  )
+
+        config_bucket.add_to_resource_policy(
+            iam.PolicyStatement(
+                actions=['s3:GetBucketAcl'],
+                effect=iam.Effect.ALLOW,
+                # principals=[config_role],
+                principals=[iam.ServicePrincipal('config.amazonaws.com')],
+                resources=[config_bucket.bucket_arn]
+            )
+        )
+
+        config_bucket.add_to_resource_policy(
+            iam.PolicyStatement(
+                actions=['s3:PutObject'],
+                effect=iam.Effect.ALLOW,
+                # principals=[config_role],
+                principals=[iam.ServicePrincipal('config.amazonaws.com')],
+                resources=[config_bucket.arn_for_objects(
+                    'AWSLogs/'+core.Stack.of(self).account+'/Config/*')],
+                conditions={"StringEquals": {
+                    's3:x-amz-acl': 'bucket-owner-full-control', }}
+            )
+        )
+
+        config_delivery_stream = config.CfnDeliveryChannel(self, "ConfigDeliveryChannel",
+                                                           s3_bucket_name=config_bucket.bucket_name
+                                                           )
